718cc33c4338c8f8aa279d60095dc717e977e651
snake-byte <snakebyte@localhost.localdomain>
2023-04-02 19:08:46 -0400

    Student Sign Up Page Made, New DB CHANGES, need to make email unique...

--- Dev_Project/javascript/Register.js
+++ Dev_Project/javascript/Register.js
@@ -11,3 +11,30 @@ function registerClass(){
     }
     req.send(formData)
 }
+
+function signUp(){
+    req = new XMLHttpRequest(); 
+    script = "sign_up.php"
+    req.open('POST',script,true)
+    var formData = new FormData(document.getElementById("sign_up")); 
+    req.onreadystatechange=function(){ 
+        if (this.readyState == 4 && this.status == 200) {
+        if(req.responseText == -2){ 
+            document.getElementById('warning').innerText="Missing Parameters!"
+        }
+        if(req.responseText == -1){ 
+            document.getElementById('warning').innerText="Fields cannot be empty!"
+        }
+        if(req.responseText == 1){ 
+            document.getElementById('warning').innerText="User Created!"
+            refreshTable(script)
+        }
+        else{ 
+            document.getElementById('warning').innerText="Possible SQL Error, try request and again check the reponse."
+        }
+        
+        }
+    };
+   
+    req.send(formData)
+}
---
+++ Dev_Project/sign_up.php
@@ -0,0 +1,131 @@
+<?php
+include("includes/db_connect.php");
+#!GOING TO HAVE TO CHANGE EMAIL TO UNIQUE IN DB!#
+if( isset($_POST['click'])){
+    if(isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['password'])){ 
+      if( !empty($_POST['fname']) && !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['password'])){
+        $fname=$_POST['fname'];
+        $lname=$_POST['lname'];
+        $email=$_POST['email'];
+        $password= password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $role=$_POST['role'];
+    
+        $stmt = $conn->prepare("INSERT INTO Users(First_Name,Last_Name,Email,Password,Role) VALUES (?,?,?,?,?)"); 
+        $role = "student";
+        $stmt->bind_param("sssss",$fname,$lname,$email,$password,$role); 
+        if(!$stmt->execute()) echo $stmt->error;
+        echo"1";
+        exit();
+    
+      }
+      else{ 
+        echo"-1";
+        exit();
+      }
+       
+    }
+    else{ 
+      echo "-2";
+      exit();
+    }
+    }
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <title>Student Sign Up</title> 
+  <h1 id=warning></h1>
+  <script src="javascript/Async.js"></script>
+  <script src="javascript/Register.js"></script>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link
+    rel="stylesheet"
+    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+    crossorigin="anonymous"
+  />
+  
+</head>
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card card-signin my-5">
+          <div class="card-body">
+            <h5 class="card-title text-center">Student Sign Up</h5>
+
+            <form onkeydown="return event.key != 'Enter';" class="form-signin" id="sign_up">
+              <div class="form-label-group">
+              <input
+                  type="fname"
+                  id="fname"
+                  for="fname"
+                  name="fname"
+                  class="form-control"
+                  placeholder="First name"
+                  required
+                  autofocus
+                />
+                <label for="fname">First name</label>
+                <input
+                  type="lname"
+                  id="lname"
+                  for="lname"
+                  name="lname"
+                  class="form-control"
+                  placeholder="Last name"
+                  required
+                  autofocus
+                />
+                <label for="lname">Last name</label>
+              
+              
+                <input
+                  type="email"
+                  id="email"
+                  for="email"
+                  name="email"
+                  class="form-control"
+                  placeholder="Email address"
+                  required
+                  autofocus
+                />
+                <label for="email">Email address</label>
+              </div>
+
+              <div class="form-label-group">
+                <input
+                  type="password"
+                  id="password"
+                  for="password"
+                  name="password"
+                  class="form-control"
+                  placeholder="Password"
+                  required
+                />
+                <label for="password">Password</label>
+              </div>
+              <input type="hidden" value="1" name="click"/>
+              <button
+                class="btn btn-lg btn-primary btn-block text-uppercase"
+                type="button"
+                onclick="signUp();"
+              >
+                Sign Up
+              </button>
+              <hr class="my-4" />
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
