534d766f1b221ded2082d6b716fbd56efe9131a8
Juan Cadile <juancadile@gmail.com>
2023-04-26 11:38:33 -0400

    more front

--- Dev_Project/manage_php/assignment_manage.php
+++ Dev_Project/manage_php/assignment_manage.php
@@ -375,22 +375,7 @@ if( isset($_POST['click'])){
       
       </div>
         </br></br>
-      <div class="card" style="height:auto !important;">
-              <div class="card-header">
-                  <h5 class="mb-0">Link Question</h5>
-              </div>
-              <div class="card-body" id="table area" style="height:100%">
-              
-  <form id="question_add">
-    <label for="assignment_id">Assignment ID:</label><br>
-    <input type="text" class="form-control" id="assignment_id" name="assignment_id"><br>
-    <label for="question_id">Question ID:</label><br>
-    <input type="text" class="form-control" id="question_id" name="question_id"><br>
-    <input type="hidden" value="1" name="question_add"/>
-    <button type="button" class="btn btn-primary" onclick="addQuestion();">Submit</button>
-  </form>
-      </div>
-
+      
   </div>
 
   
@@ -518,14 +503,30 @@ if( isset($_POST['click'])){
 </div>
 
 
+
   
   </br> 
   </br>
+  <div class="card" style="height:auto !important;">
+              <div class="card-header">
+                  <h5 class="mb-0">Link Question</h5>
+              </div>
+              <div class="card-body" id="table area" style="height:100%">
+              
+  <form id="question_add">
+    <label for="assignment_id">Assignment ID:</label><br>
+    <input type="text" class="form-control" id="assignment_id" name="assignment_id"><br>
+    <label for="question_id">Question ID:</label><br>
+    <input type="text" class="form-control" id="question_id" name="question_id"><br>
+    <input type="hidden" value="1" name="question_add"/>
+    <button type="button" class="btn btn-primary" onclick="addQuestion();">Submit</button>
+  </form>
+      </div></div>
 
   </br> 
   </br>
 
-  </div>
+</div>
   <!-- jQuery -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <!-- Popper.js -->
